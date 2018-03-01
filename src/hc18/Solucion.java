7be1dcab97cb7457583a1b2b902717579366c27a
josebummer <jantonioruiz@correo.ugr.es>
2018-03-01 19:38:36 +0100

    funciones

--- src/hc18/Solucion.java
+++ src/hc18/Solucion.java
@@ -1,16 +1,22 @@
+import java.util.Random;
+ 
+public class Solucion{
 //Guillermo ini
-public boolean validar(Trayecto t, int c){
+    public boolean validar(int t, int c){
 
+    }
+    //G end
+    //J ini
+    public List<Objet> crear_hermano(){
+        Random r = new Random();
+        while(true){
+            int t = r.nexRandom(5);
+        }
+    }
+    //J end
+    //A ini
+    public void aplicar_cambios(Trayecto t, int c){
+
+    }
+    //A end
 }
-//G end
-//J ini
-public List<Objet> crear_hermano(){
-
-}
-//J end
-//A ini
-public void aplicar_cambios(Trayecto t, int c){
-
-}
-//A end
-
