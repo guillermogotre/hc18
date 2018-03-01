748747bee503c1e0b8cd9f6ddfb87b0fb479f940
josebummer <jantonioruiz@correo.ugr.es>
2018-03-01 19:35:29 +0100

    funciones

--- src/hc18/Solucion.java
+++ src/hc18/Solucion.java
@@ -1,11 +1,16 @@
 //Guillermo ini
+public boolean validar(Trayecto t, int c){
 
-
+}
 //G end
 //J ini
+public List<Objet> crear_hermano(){
 
+}
 //J end
 //A ini
+public void aplicar_cambios(Trayecto t, int c){
 
+}
 //A end
 
